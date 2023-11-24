3a9c7b0f09f1c68433acd33fe0fe0e39667af4c2
aptclouds0039 <126168319+aptclouds0039@users.noreply.github.com>
2023-11-24 14:14:47 +0530

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, text
+
+user = 'sql12664669'
+password = 'X6xTDuwSmr'
+host = 'sql12.freesqldatabase.com'
+database = 'sql12664669'
+# Create an SQLAlchemy engine and start a transaction
+connection_string = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+engine = create_engine(connection_string)
+connection = engine.connect()
+transaction = connection.begin()
+
+
+
+def lambda_handler(event, context):
+    try:
+        result = connection.execute(text("SELECT * FROM Destinations"))
+        print(result);
+        for row in result:
+            print(row)
+        transaction.commit()
+    except Exception as e:
+        print(e);
+        transaction.rollback()  # Roll back the transaction if an exception occurs
+    finally:
+        connection.close()
