28f7ef30c99005945970cd62e32daaed28cd5612
Nishant Redu <nishantr@aptclouds.com>
2023-11-24 17:13:07 +0530

    Implemented Connection Pooling

--- lambda_function.py
+++ lambda_function.py
@@ -1,27 +1,28 @@
 from sqlalchemy import create_engine, text
-# Testing Branch
+from sqlalchemy.pool import QueuePool
+import json
+
 user = 'sql12664669'
 password = 'X6xTDuwSmr'
 host = 'sql12.freesqldatabase.com'
 database = 'sql12664669'
-# Create an SQLAlchemy engine and start a transaction
+
 connection_string = f'mysql+pymysql://{user}:{password}@{host}/{database}'
-engine = create_engine(connection_string)
-connection = engine.connect()
-transaction = connection.begin()
-
-
+engine = create_engine(connection_string, poolclass=QueuePool, pool_size=5, max_overflow=10)
 
 def lambda_handler(event, context):
     try:
-        result = connection.execute(text("SELECT * FROM Destinations"))
-        print(result);
-        for row in result:
-            print(row)
-        transaction.commit();
-        return result;
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT * FROM Destinations"))
+            allresults = result.mappings().all();
+            finalResults = [dict(row) for row in allresults]
+            return {
+                'statusCode': 200,
+                'body': finalResults
+            }
     except Exception as e:
-        print(e);
-        transaction.rollback()  # Roll back the transaction if an exception occurs
-    finally:
-        connection.close()
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
